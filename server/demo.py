6646946361aa1f7c8183f82f07a409e9b00d2774
jjangga0214 <jjangga@kookmin.ac.kr>
2017-10-07 16:43:42 +0900

    fix minor bug on server/demo.py
    fix missed arg on demo

--- server/demo.py
+++ server/demo.py
@@ -4,6 +4,14 @@ import time
 if __name__ == "__main__":
     car = rcar.RCar()
 
-    car.forward()
+    car.forward(60)
+    time.sleep(3)
+    car.stop()
+    time.sleep(1)
+    car.backward(60)
+    time.sleep(3)
+    car.stop()
+    time.sleep(1)
+    car.forward(-60)
     time.sleep(3)
     car.stop()
